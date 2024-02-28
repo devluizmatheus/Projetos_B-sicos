3009b5dadd6f5faadf9c8bbe6c7f044d363d3ed9
Luiz Matheus de Oliveira Leite <154921255+devluizmatheus@users.noreply.github.com>
2024-02-28 10:48:48 -0300

    Verification_password.py

---
+++ Verification_password.py
@@ -0,0 +1,14 @@
+import re 
+
+
+def password(message):
+    enter = input('Enter a password that has the following requeriments:\n' 
+    '1° Your password must have uppercase and lowercase letters.\n'
+    '2° Your password must have numbers.\n'
+    '=> ')
+    enter = message
+
+    return enter
+
+    def verification(message):
+        if message in 
