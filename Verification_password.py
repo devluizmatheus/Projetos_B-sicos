a228a4ab241d23b2902628ed46059092ee017e33
Luiz Matheus de Oliveira Leite <154921255+devluizmatheus@users.noreply.github.com>
2024-02-29 08:00:38 -0300

    Verification_password.py

--- Verification_password.py
+++ Verification_password.py
@@ -1,14 +1,23 @@
 import re 
 
 
-def password(message):
+def password():
+    global enter
     enter = input('Enter a password that has the following requeriments:\n' 
     '1° Your password must have uppercase and lowercase letters.\n'
     '2° Your password must have numbers.\n'
     '=> ')
-    enter = message
 
-    return enter
+    verification()
 
-    def verification(message):
-        if message in 
+def verification():
+         
+        if re.search('[a-z]', enter) and re.search('[A-Z]', enter) and re.search('[0-9]', enter):
+            print('Your password has been accepted')
+
+        else:
+            print("Your password has been refused")
+            password
+
+
+password()
